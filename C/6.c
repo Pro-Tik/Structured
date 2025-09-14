0ac13e9a0a25230955a0c8d1b86ec9b9c8da6bb6
Pro-Tik <baruaprathik06@gmail.com>
2025-09-14 20:01:28 +0000

    Sep 15, 2025, 2:01 AM

--- C/6.c
+++ C/6.c
@@ -3,10 +3,8 @@ int main(){
     int a,i;
     scanf("%d",&a);
     for(i=0;i<=a;i++){
-        if(a>=1 && a<=1000){
-            if(a%2 != 0){
-                printf("%d",a);
-            }
+        if(a>=1 && a<=1000 && a%2 != 0){
+            printf("%d",a);
         }
     }
 }
