b6eb14444892416ffc98ce0deab44d46d4ac34c6
Pro-Tik <baruaprathik06@gmail.com>
2025-09-14 20:31:39 +0000

    Sep 15, 2025, 2:31 AM

--- C/7.c
+++ C/7.c
@@ -1,23 +1,23 @@
-#include<stdio.h>
-int main(){
-    int N,x,i;
-    scanf("%d",&N);
-    for(i=0;i<N && N<1000;i++){
-    scanf("%d",&x);
-    if(x>0 && x%2 != 0){
-        printf("ODD POSITIVE\n");
+#include <stdio.h>
+
+int main(void) {
+    int N, x, i;
+
+    if (scanf("%d", &N) != 1) return 0;   // read number of testcases
+    // optionally validate N: problem says N < 10000
+    if (N < 0) return 0;
+
+    for (i = 0; i < N; ++i) {
+        if (scanf("%d", &x) != 1) break;
+
+        if (x == 0) {
+            printf("NULL\n");
+        } else if (x % 2 == 0) {                  // even
+            printf("EVEN %s\n", x > 0 ? "POSITIVE" : "NEGATIVE");
+        } else {                                  // odd
+            printf("ODD %s\n", x > 0 ? "POSITIVE" : "NEGATIVE");
+        }
     }
-    if(x<0 && x%2 != 0){
-        printf("ODD NEGATIVE\n");
-    }
-    if(x>0 && x%2 == 0){
-        printf("EVEN POSITIVE\n");
-    }
-    if(x<0 && x%2 == 0){
-        printf("EVEN NEGATIVE\n");
-    }
-    if(x==0){
-        printf("NULL\n");
-    }
-    }
+
+    return 0;
 }
