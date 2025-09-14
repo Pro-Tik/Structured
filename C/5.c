66cf41df750d3fef3241be4c22eb9aa4129c928b
Pro-Tik <baruaprathik06@gmail.com>
2025-09-14 19:09:23 +0000

    Sep 15, 2025, 1:09 AM

--- C/5.c
+++ C/5.c
@@ -3,9 +3,10 @@ int main(){
     long long a;
     int i;
     scanf("%lld",&a);
-    for(i=0;i<=a-1;i++){
+    if(a>0 && a<= 1000000){
+        for(i=0;i<=a-1;i++){
         printf("Ho ");
     }
-    
+}
     printf("Ho!");
 }
