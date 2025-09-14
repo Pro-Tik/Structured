0e4f1ec5068cf391e487e2ff0e3f1410f235d723
Pro-Tik <baruaprathik06@gmail.com>
2025-09-14 20:26:31 +0000

    Sep 15, 2025, 2:26 AM

--- C/7.c
+++ C/7.c
@@ -2,19 +2,19 @@
 int main(){
     int N,x,i;
     scanf("%d",&N);
-    for(i=0;i<=N;i++){
+    for(i=0;i<N;i++){
     scanf("%d",&x);
     if(x>0 && x%2 != 0){
         printf("ODD POSITIVE\n");
     }
     if(x<0 && x%2 != 0){
-        printf("ODD NEGATVE\n");
+        printf("ODD NEGATIVE\n");
     }
     if(x>0 && x%2 == 0){
         printf("EVEN POSITIVE\n");
     }
     if(x<0 && x%2 == 0){
-        printf("EVEN NEGATVE\n");
+        printf("EVEN NEGATIVE\n");
     }
     if(x==0){
         printf("NULL\n");
