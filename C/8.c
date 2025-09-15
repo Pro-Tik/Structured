e143def182d9ff305550368d0d3317ff88a51fbc
Pro-Tik <baruaprathik06@gmail.com>
2025-09-15 20:02:41 +0000

    Sep 16, 2025, 2:02 AM

---
+++ C/8.c
@@ -0,0 +1,14 @@
+//https://judge.beecrowd.com/en/problems/view/1071
+#include<stdio.h>
+int main(){
+    int a,b,i,sum;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    for(i=a;i<b;i++){
+        if(i%2 != 0){
+            sum = sum +i;
+        }
+
+    }
+    printf("%d",sum);
+}
