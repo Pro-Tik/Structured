231d77d8edd41a9c3f696e212521f2b781ca9146
Pro-Tik <baruaprathik06@gmail.com>
2025-09-15 20:21:17 +0000

    Sep 16, 2025, 2:21 AM

--- C/8.c
+++ C/8.c
@@ -1,7 +1,8 @@
 //https://judge.beecrowd.com/en/problems/view/1071
 #include<stdio.h>
 int main(){
-    int a,b,i,sum,temp;
+    int a,b,i,temp;
+    int sum=0;
     scanf("%d",&a);
     scanf("%d",&b);
     if(a>b){
@@ -15,8 +16,6 @@ int main(){
         }
 
     }
-    printf("%d\n",a);
-    printf("%d\n",b);
-    printf("%d\n",i);
+
     printf("%d\n",sum);
 }
