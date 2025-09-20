934efa1a4acc511782cd2db9ec50c5bd0ccc84c4
Pro-Tik <baruaprathik06@gmail.com>
2025-09-20 04:49:40 +0000

    Sep 20, 2025, 10:49 AM

---
+++ Beecrowd/1176.c
@@ -0,0 +1,21 @@
+//https://judge.beecrowd.com/en/problems/view/1176
+
+
+#include<stdio.h>
+int main(){
+    int a,i,N,t1,t2;
+    scanf("%d",&N);
+    t1=0;
+    t2 = 1;
+    for(i=0;i<N;i++){
+        scanf("%d",&a);
+        t1=t2;
+        printf("%d\n",t1);
+        t2= t1 + t2;
+        printf("%d",t2);
+
+
+
+    }
+
+}
