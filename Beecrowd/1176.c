eb851170df6f837beec5c105bb17f5c845097051
Pro-Tik <baruaprathik06@gmail.com>
2025-09-20 04:55:56 +0000

    Sep 20, 2025, 10:55 AM

--- Beecrowd/1176.c
+++ Beecrowd/1176.c
@@ -9,11 +9,14 @@ int main(){
     t2 = 1;
     for(i=0;i<N;i++){
         
-        t1=t2;
+        
         printf("%d\n",t1);
         printf("%d\n",t2);
         t1=t2;
         t1= t1 + t2;
+        printf("%d\n",t1);
+        printf("%d\n",t2);
+
         
 
 
